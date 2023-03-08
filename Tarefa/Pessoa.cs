dc76e529f807c255a9ecff3d55e0b898a2a19fc0
IzaEdits <izaedits29@gmail.com>
2023-03-08 10:08:53 -0300

    Add project files.

---
+++ Tarefa/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarefa
+{
+    public class Pessoa
+    {
+        public Pessoa(string nome, string telefone, int id)
+        {
+            Id = id;
+        }
+    }
+}
