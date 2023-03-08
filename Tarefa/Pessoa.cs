304b286dcf152e8bb10c7b03270caf71448f28e3
IzaEdits <izaedits29@gmail.com>
2023-03-08 10:19:57 -0300

    Criei os atributos da classe e desenvolvi os métodos get e set

--- Tarefa/Pessoa.cs
+++ Tarefa/Pessoa.cs
@@ -8,9 +8,52 @@ namespace Tarefa
 {
     public class Pessoa
     {
+        public int Id;
+        public string Nome;
+        public string Telefone;
+
         public Pessoa(string nome, string telefone, int id)
         {
             Id = id;
+            Nome = nome;
+            Telefone = telefone;
         }
+
+        public void setId(int id)
+        {
+            Id = id;
+        }
+
+        public int getId()
+        {
+            return Id;
+        }
+
+        public void setNome(string nome)
+        {
+            Nome = nome;
+        }
+
+        public string getNome()
+        {
+            return Nome;
+        }
+
+        public void setTelefone(string telefone)
+        {
+            Telefone = telefone;
+        }
+
+        public string getTelefone()
+        {
+            return Telefone;
+        }
+
+
+
+
+
+
+
     }
 }
